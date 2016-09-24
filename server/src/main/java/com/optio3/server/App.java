b18b705c13f099f959dc36736323bb4137369850
jakeoptio3 <jakeoptio3@gmail.com>
2016-09-23 17:35:49 -0700

    Server Initial commit

---
+++ server/src/main/java/com/optio3/server/App.java
@@ -0,0 +1,13 @@
+package com.optio3.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
